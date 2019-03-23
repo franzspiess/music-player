8a00e9f8957e87256176abed4bb61272907723d0
franzspiess <picnhepanchopincho@gmail.com>
2019-03-23 16:04:33 +0100

    Updates

--- precache-manifest.fb67de3373ec0878c6079012a275c6d0.js
+++ precache-manifest.fb67de3373ec0878c6079012a275c6d0.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/music-player/static/js/runtime~main.fdd2de90.js"
   },
   {
-    "revision": "152ee022deb6a57ba7fe",
-    "url": "/music-player/static/js/main.e23a0db5.chunk.js"
+    "revision": "0c16d11f8cdc982389b0",
+    "url": "/music-player/static/js/main.e1613518.chunk.js"
   },
   {
     "revision": "65b35fd65c29f298dcaa",
     "url": "/music-player/static/js/2.87c04a7c.chunk.js"
   },
   {
-    "revision": "152ee022deb6a57ba7fe",
+    "revision": "0c16d11f8cdc982389b0",
     "url": "/music-player/static/css/main.622f5d4e.chunk.css"
   },
   {
-    "revision": "4b208c81dbcbc4f828e4b6649d2b233f",
+    "revision": "b8b729be94a8c8d22f0a3aab30426c5a",
     "url": "/music-player/index.html"
   }
 ];
